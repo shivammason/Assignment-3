a3c0b109eaf2d88e25177e9bbef26800c99ad6c1
Shivam Masaon <200426022@student.georgianc.on.ca>
2019-05-30 16:13:58 -0400

    Update Assignment 3 Q5.py

--- Assignment 3 Q5.py
+++ Assignment 3 Q5.py
@@ -1,9 +1,41 @@
-my_list = []
-fhand = open('romeo.txt')
-for line in fhand:
-    words = line.split()
-    for word in words:
-        if word in my_list:
-            continue
-        my_list.append(word)
-print(sorted(my_list))
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list0 = []
+list1 = []
+list2 = []
+list3 = []
+mainList = []
+file = input("Enter file: ")
+fhand = open(file)
+lines = fhand.read()
+for line in lines:
+    line = lines.split('\n')
+    for word in line[0]:
+        list0 = line[0].split(' ')
+    for word in line[1]:
+        list1 = line[1].split(' ')
+    for word in line[2]:
+        list2 = line[2].split(' ')
+    for word in line[3]:
+        list3 = line[3].split(' ')
+mainList = list0+list1+list2+list3;
+mainList = sorted(mainList)
+print(mainList)
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
