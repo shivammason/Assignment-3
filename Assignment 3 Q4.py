2b8d1ee058b0dbc270bc6e2eaf87da18102e368b
Chitresh <Chitresh.Sharma@georgiancollege.ca>
2019-05-30 01:58:02 -0400

    Create Assignment 3 Q4.py

---
+++ Assignment 3 Q4.py
@@ -0,0 +1,20 @@
+count = 0
+total = 0
+
+filename = input('Enter the file name: ')
+try:
+    filehand = open(filename)
+except FileNotFoundError:
+    print('File cannot be opened: ', filename)
+    quit()
+
+for line in fhand:
+    if line.startswith('X-DSPAM-Confidence: '):
+        count = count + 1
+        colpos = line.find(':')
+        number = line[colpos+1:].strip()
+        SPAM_C = float(number)
+        total = total + SPAM_C
+
+average = total / count
+print('Average spam confidence: ', average)
