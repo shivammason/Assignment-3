3dee519a4c675f2aa8957d551645129e12a07d4e
shivammason <50720119+shivammason@users.noreply.github.com>
2019-05-30 16:11:48 -0400

    Delete Assignment 3 Q5.py

--- Assignment 3 Q5.py
+++
@@ -1,9 +0,0 @@
-my_list = []
-fhand = open('romeo.txt')
-for line in fhand:
-    words = line.split()
-    for word in words:
-        if word in my_list:
-            continue
-        my_list.append(word)
-print(sorted(my_list))
